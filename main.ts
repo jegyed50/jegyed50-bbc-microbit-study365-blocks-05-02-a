7282d44b739b88b9b8f1e269eaf2f01d57c8b5b0
Egyed József <jegyed50@gmail.com>
2023-06-01 08:27:33 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
-//The Magnetometer Module Blocks
+/**
+ * The Magnetometer Module Blocks, Magnetic Force Stength value to Graph
+ */
+let mf = 0
+input.setAccelerometerRange(AcceleratorRange.OneG)
+if (input.compassHeading() == -1003) {
+    basic.showIcon(IconNames.No)
+} else {
+    basic.showIcon(IconNames.Yes)
+}
+basic.pause(3000)
 basic.forever(function () {
-	
+    mf = input.magneticForce(Dimension.Strength)
+    serial.writeValue("mf", mf)
+    led.plotBarGraph(
+    mf / 2000,
+    0
+    )
 })
